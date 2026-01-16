c43566e0c84490a846cd027c22f1bff3fdae0178
kethak <kethakphayade@gmail.com>
2026-01-16 17:37:27 +0530

    domain Update

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -23,7 +23,7 @@ DEBUG = env.bool("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "web-production-a8cff.up.railway.app"]
 
-CSRF_TRUSTED_ORIGINS = ["web-production-a8cff.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://web-production-a8cff.up.railway.app"]
 
 # Application definition
 
